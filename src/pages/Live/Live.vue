62b11d1c4cca0e9140d29b3927e3e659f15c55ad
fayu <1105565324@qq.com>
2019-06-18 14:26:48 +0800

    test

--- src/pages/Live/Live.vue
+++ src/pages/Live/Live.vue
@@ -1,17 +1,26 @@
 <template>
-    <div>
-      <Header :title="'直播'"></Header>
-    </div>
+  <div>
+    <Header :title="'直播'"></Header>
+    <el-row class="livePage">
+      <el-row class="title">测试使用</el-row>
+    </el-row>
+  </div>
 </template>
 
 <script>
-    import Header from "../../components/Header/Header";
-    export default {
-        name: "Live",
-      components: {Header}
-    }
+import Header from '../../components/Header/Header'
+
+export default {
+  name: 'Live',
+  components: { Header }
+}
 </script>
 
-<style scoped>
-
+<style lang="scss">
+.livePage {
+  .title{
+    font-size: 30px;
+    color: red;
+  }
+}
 </style>
